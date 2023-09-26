52dfca5d1b939d1e75bef405732cf06a43d03e16
Artiom_Sokolov <sokolova-net@mail.ru>
2023-09-26 21:36:04 +0300

    add js user header menu

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,17 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+
+// Header
+const userHeader = document.querySelector('.user-header');
+if (userHeader) {
+  document.addEventListener("click", function (e) {
+    const targetElement = e.target;
+    if (targetElement.closest('.user-header__arrow')) {
+      userHeader.classList.toggle('user-header_open');
+    } else if (!targetElement.closest('.user-header')) {
+      userHeader.classList.remove('user-header_open');
+    }
+  });
+}
