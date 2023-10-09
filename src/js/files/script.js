18d55f9b9fae1406d30a7e4e535bcbfec52b32a8
Artiom_Sokolov <sokolova-net@mail.ru>
2023-10-09 14:54:16 +0300

    media querries for header

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -16,3 +16,15 @@ if (userHeader) {
     }
   });
 }
+
+const search = document.querySelector('.search');
+if (search) {
+  document.addEventListener("click", function (e) {
+    const targetElement = e.target;
+    if (targetElement.closest('.search__icon')) {
+      search.classList.add('search__open');
+    } else if (!targetElement.closest('.search')) {
+      search.classList.remove('search__open');
+    }
+  });
+}
--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -79,27 +79,22 @@ function initSliders() {
       },
 
       // Брейкпоинты
-      /*
+
       breakpoints: {
         320: {
-          slidesPerView: 1,
-          spaceBetween: 0,
-          autoHeight: true,
+
         },
         768: {
-          slidesPerView: 2,
-          spaceBetween: 20,
+
         },
         992: {
-          slidesPerView: 3,
-          spaceBetween: 20,
-        },
-        1268: {
           slidesPerView: 4,
-          spaceBetween: 30,
+        },
+        1100: {
+          slidesPerView: 5,
         },
       },
-      */
+
       // События
       on: {
 
