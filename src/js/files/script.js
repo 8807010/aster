3cc9624ab088c548b81b2e30a65d1afd975b67bd
Artiom_Sokolov <sokolova-net@mail.ru>
2023-10-18 22:38:57 +0300

    weather section with api weather

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -102,11 +102,11 @@ const weatherBlock = document.querySelector('#weather');
 
 async function loadWeather(e) {
   weatherBlock.innerHTML = `
-		<div class="weather__loading">
-			<img src="img/loading.gif" alt="Loading...">
-		</div>`;
+    <div class="weather__loading">
+      <img style="fill:blue" src="img/loading.gif" alt="Loading...">
+    </div>`;
 
-  const server = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=Kyiv&appid=522f7ec766b55c89fccbc47a4e7a72c0';
+  const server = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=Minsk&appid=771462bf640c00d0105b972d46ef71d8';
   const response = await fetch(server, {
     method: 'GET',
   });
@@ -120,26 +120,30 @@ async function loadWeather(e) {
 }
 
 function getWeather(data) {
+  // console.log(data);
   // Обробляємо та виводимо данні
   const location = data.name;
   const temp = Math.round(data.main.temp);
-  const feelsLike = Math.round(data.main.feels_like);
+  // const feelsLike = Math.round(data.main.feels_like);
   const weatherStatus = data.weather[0].main;
   const weatherIcon = data.weather[0].icon;
 
   // HTML шаблон
   const template = `
-	<div class="weather__header">
-		<div class="weather__main">
-			<div class="weather__city">${location}</div>
-			<div class="weather__status">${weatherStatus}</div>
-		</div>
-		<div class="weather__icon">
-			<img src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="${weatherStatus}">
-		</div>
-	</div>
-	<div class="weather__temp">${temp}</div>
-	<div class="weather__feels-like">Feels like: ${feelsLike}</div>`;
+  <div class="weather-module__title _icon-crosshair">${location}</div>
+   <div class="weather-module__body body-weather-module">
+   <div class="body-weather-module__info">
+     <h4 class="body-weather-module__status">${weatherStatus}</h4>
+     <div class="body-weather-module__value">${temp}<sup>o</sup>c</div>
+   </div>
+   <div class="body-weather-module__icon">
+     <img src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="${weatherStatus}">
+   </div>
+ </div>
+ <div class="weather-module__footer">
+   <button type="button" class="weather-module__type weather-module__type_active">Celsius</button>
+   <button type="button" class="weather-module__type">Fahrenheit</button>
+ </div>`;
 
   weatherBlock.innerHTML = template;
 
