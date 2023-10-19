7339e61cf3bbf604cf93c567bb67f3bca9ec0f0b
Artiom_Sokolov <sokolova-net@mail.ru>
2023-10-19 23:11:00 +0300

    fix some bugs

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -28,77 +28,77 @@ if (search) {
   });
 }
 
-const moreNews = document.querySelector('.news-module__buttton');
-moreNews.addEventListener("click", loadNews);
+// const moreNews = document.querySelector('.news-module__buttton');
+// moreNews.addEventListener("click", loadNews);
 
-async function loadNews(e) {
-  const server = 'files/data/news.json';
-  const response = await fetch(server, {
-    method: 'GET',
-  });
-  if (response.ok) {
-    const responseResult = await response.json();
-    newsBuild(responseResult);
-  } else {
-    alert("Error");
-  }
-  e.preventDefault();
-}
+// async function loadNews(e) {
+//   const server = 'files/data/news.json';
+//   const response = await fetch(server, {
+//     method: 'GET',
+//   });
+//   if (response.ok) {
+//     const responseResult = await response.json();
+//     newsBuild(responseResult);
+//   } else {
+//     alert("Error");
+//   }
+//   e.preventDefault();
+// }
 
-const template =
-  `<article class="news-module__item item %noimage%">
-		<div class="item__body">
-			<h4 class="item__title">
-				<a href="#" class="item__link-title">%title%</a>
-			</h4>
-			<div class="item__text">%text%</div>
-		</div>
-		%imagePlace%
-		<div class="item__footer">
-			<div class="item__info info-item">
-				<a href="" class="info-item__category">%category%</a>
-				<div class="info-item__time">%publish%</div>
-			</div>
-			<div class="item__actions actions-item">
-				<a href="%share%" class="actions-item__link _icon-share">Share</a>
-				<a href="%read%" class="actions-item__link _icon-read">Read Later</a>
-			</div>
-		</div>
-	</article>`;
+// const template =
+//   `<article class="news-module__item item %noimage%">
+// 		<div class="item__body">
+// 			<h4 class="item__title">
+// 				<a href="#" class="item__link-title">%title%</a>
+// 			</h4>
+// 			<div class="item__text">%text%</div>
+// 		</div>
+// 		%imagePlace%
+// 		<div class="item__footer">
+// 			<div class="item__info info-item">
+// 				<a href="" class="info-item__category">%category%</a>
+// 				<div class="info-item__time">%publish%</div>
+// 			</div>
+// 			<div class="item__actions actions-item">
+// 				<a href="%share%" class="actions-item__link _icon-share">Share</a>
+// 				<a href="%read%" class="actions-item__link _icon-read">Read Later</a>
+// 			</div>
+// 		</div>
+// 	</article>`;
 
-const templateImage = `
-	<a href="#" class="item__image-ibg">
-		<img src="img/%image%" alt="">
-	</a>`;
+// const templateImage = `
+// 	<a href="#" class="item__image-ibg">
+// 		<img src="img/%image%" alt="">
+// 	</a>`;
 
-function newsBuild(data) {
-  const news = data.news;
-  if (news.length) {
-    const blockItems = document.querySelector('.news-module__items');
-    news.forEach(newsItem => {
-      let templateItem = template;
-      let templateImageItem = templateImage;
-      for (const key in newsItem) {
-        templateItem = templateItem.replace(`%${key}%`, newsItem[key]);
-      }
-      if (!newsItem.image) {
-        templateItem = templateItem.replace(`%noimage%`, 'item_noimage');
-        templateItem = templateItem.replace(`%imagePlace%`, '');
-      } else {
-        templateItem = templateItem.replace(`%noimage%`, '');
-        templateImageItem = templateImageItem.replace(`%image%`, newsItem.image);
-        templateItem = templateItem.replace(`%imagePlace%`, templateImageItem);
-      }
-      blockItems.insertAdjacentHTML('beforeend', templateItem);
-    });
-  } else {
-    console.log("No data");
-  }
-}
+// function newsBuild(data) {
+//   const news = data.news;
+//   if (news.length) {
+//     const blockItems = document.querySelector('.news-module__items');
+//     news.forEach(newsItem => {
+//       let templateItem = template;
+//       let templateImageItem = templateImage;
+//       for (const key in newsItem) {
+//         templateItem = templateItem.replace(`%${key}%`, newsItem[key]);
+//       }
+//       if (!newsItem.image) {
+//         templateItem = templateItem.replace(`%noimage%`, 'item_noimage');
+//         templateItem = templateItem.replace(`%imagePlace%`, '');
+//       } else {
+//         templateItem = templateItem.replace(`%noimage%`, '');
+//         templateImageItem = templateImageItem.replace(`%image%`, newsItem.image);
+//         templateItem = templateItem.replace(`%imagePlace%`, templateImageItem);
+//       }
+//       blockItems.insertAdjacentHTML('beforeend', templateItem);
+//     });
+//   } else {
+//     console.log("No data");
+//   }
+// }
 
 
 // Блок з погодою
-const weatherBlock = document.querySelector('#weather');
+const weatherBlock = document.querySelector('.weather-module');
 
 async function loadWeather(e) {
   weatherBlock.innerHTML = `
